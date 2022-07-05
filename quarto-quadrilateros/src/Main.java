8081220513018b60c0d4946c0da5ca08642bc31c
joicereis <joice.jdosreis@gmail.com>
2022-07-04 22:52:55 -0300

    Ecercicio de Retornos de metodos- Quadrilateros

--- quarto-quadrilateros/src/Main.java
+++ quarto-quadrilateros/src/Main.java
@@ -4,9 +4,16 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Exercício de quadriláteros.");
-		Quadrilatero.area(3);
-		Quadrilatero.area(3d, 4d);
-		Quadrilatero.area(6, 3, 4);
-		Quadrilatero.area(7f, 8f);
+		double areaQuadrado = Quadrilatero.area(3);
+		System.out.println("Área do quadrado: " + areaQuadrado);
+		
+		double areaRetangulo = Quadrilatero.area(3d, 4d);
+		System.out.println("Área do quadrado: " + areaRetangulo);
+		
+		double areaTrapezio = Quadrilatero.area(6, 3, 4);
+		System.out.println("Área do quadrado: " + areaTrapezio);
+		
+		double areaLosango = Quadrilatero.area(7f, 8f);
+		System.out.println("Área do quadrado: " + areaLosango);
 	}
 }
--- quarto-quadrilateros/src/Quadrilatero.java
+++ quarto-quadrilateros/src/Quadrilatero.java
@@ -4,21 +4,20 @@
 
 public class Quadrilatero {
 	//SOBRECARGA 1:
-	public static void area(double lado) {
-		System.out.println("Área do quadrado: " + lado * lado);
+	public static double area(double lado) {
+		return (lado * lado);
 	}
 	//SOBRECARGA 2:
-	public static void area(double altura, double largura) {
-		System.out.println("Área do retângulo: " + altura * largura);
+	public static double area(double altura, double largura) {
+		return (altura * largura);
 	}
 	//SOBRECARGA 3:
-	public static void area(double baseMaior, double baseMenor, double altura) {
-		System.out.println("Área do trapézio: " + ((baseMaior + baseMenor) * altura) / 2);		
+	public static double area(double baseMaior, double baseMenor, double altura) {
+		return ((baseMaior + baseMenor) * altura) / 2;	
 	}
 	//SOBRECARGA 4
 	//O que difere essa sobrecarga da 2 são os tipos de dados dos parâmetros que devem ser sinalizados na msg de chamada.
-
-	public static void area(float diagonal1, float diagonal2) {
-		System.out.println("Área do losango: " + (diagonal1 + diagonal2) / 2);		
+	public static double area(float diagonal1, float diagonal2) {
+		return (diagonal1 + diagonal2) / 2;	
 	}
 }
